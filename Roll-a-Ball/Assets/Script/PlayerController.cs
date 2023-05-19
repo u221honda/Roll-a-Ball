deca8aaba320e54f4378007b07d2ceba18b0df3c
u221honda <22111326ih@tama.ac.jp>
2023-05-19 14:33:11 +0900

    Unity導入

--- Roll-a-Ball/Assets/Script/PlayerController.cs
+++ Roll-a-Ball/Assets/Script/PlayerController.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20; // 動く速さ
+    public float speed; // 動く速さ
+    public Text scoreText; // スコアの UI
+    public Text winText; // リザルトの UI
 
     private Rigidbody rb; // Rididbody
+    private int score; // スコア
 
     void Start()
     {
         // Rigidbody を取得
         rb = GetComponent<Rigidbody>();
+
+        // UI を初期化
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void Update()
@@ -23,5 +32,36 @@ public class PlayerController : MonoBehaviour
 
         // Ridigbody に力を与えて玉を動かす
         rb.AddForce(movement * speed);
+    }
+
+    // 玉が他のオブジェクトにぶつかった時に呼び出される
+    void OnTriggerEnter(Collider other)
+    {
+        // ぶつかったオブジェクトが収集アイテムだった場合
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            // その収集アイテムを非表示にします
+            other.gameObject.SetActive(false);
+
+            // スコアを加算します
+            score = score + 1;
+
+            // UI の表示を更新します
+            SetCountText ();
+        }
+    }
+
+    // UI の表示を更新する
+    void SetCountText()
+    {
+        // スコアの表示を更新
+        scoreText.text = "Count: " + score.ToString();
+
+        // すべての収集アイテムを獲得した場合
+        if (score >= 12)
+        {
+            // リザルトの表示を更新
+            winText.text = "You Win!";
+        }
     }
 }
